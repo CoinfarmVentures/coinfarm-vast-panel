ff24493e7300d617c12124b72b599262d288657a
CoinfarmVentures <rohith@coinfarm.us>
2023-06-11 17:01:13 -0400

    No more setup.sh after update

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 git fetch --all
 git reset --hard origin/master
 chmod 700 -R setup.sh update.sh build.sh restart.sh
-echo "Update complete. You may have to run setup.sh again"
+echo "Update complete. You probably have to run restart.sh"
