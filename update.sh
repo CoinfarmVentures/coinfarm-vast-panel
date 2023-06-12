9ef0a56f578a6b19d22c6657c925287e319cc45c
CoinfarmVentures <rohith@coinfarm.us>
2023-06-11 22:16:07 -0400

    Fixed update permission bug

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git fetch --all
 git reset --hard origin/master
+chmod -R 777 grafana/data
+chmod -R 777 grafana/provisioning
 chmod 700 -R setup.sh update.sh build.sh restart.sh
 echo "Update complete. You probably have to run restart.sh"
